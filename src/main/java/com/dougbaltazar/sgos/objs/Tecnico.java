590d019f43e6ca7f864ae9025ccf224491a0841c
Douglas Baltazar <douglasbaltazar1@gmail.com>
2019-11-08 18:17:48 -0200

    First Commit to my Git

---
+++ src/main/java/com/dougbaltazar/sgos/objs/Tecnico.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.dougbaltazar.sgos.objs;
+
+/**
+ *
+ * @author Cliente
+ */
+public class Tecnico {
+    private int id;
+    private String nome;
+    private int trabalhos;
+    private String status;
+    
+}
