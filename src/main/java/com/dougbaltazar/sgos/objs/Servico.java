96f8775c7b81e6608bea70f7dd115fb47e80ca24
Douglas Baltazar <douglasbaltazar1@gmail.com>
2019-11-10 14:41:49 -0200

    Modifying classes

--- src/main/java/com/dougbaltazar/sgos/objs/Servico.java
+++ src/main/java/com/dougbaltazar/sgos/objs/Servico.java
@@ -10,5 +10,141 @@ package com.dougbaltazar.sgos.objs;
  * @author Cliente
  */
 public class Servico {
+    // Cliente, Tecnico, Preço, Prioridade, Tipo ( Sistema ou Maquina ), Status, Data/Hora, Descrição, Peças ( se necessario )
+    private Cliente cliente;
+    private Tecnico tecnico;
+    private double valor;
+    private int prioridade;
+    private String tipo;
+    private String status;
+    private String dataHora;
+    private String descricao;
+    private String pecas;
+
+    /**
+     * @return the cliente
+     */
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    /**
+     * @param cliente the cliente to set
+     */
+    public void setCliente(Cliente cliente) {
+        this.cliente = cliente;
+    }
+
+    /**
+     * @return the tecnico
+     */
+    public Tecnico getTecnico() {
+        return tecnico;
+    }
+
+    /**
+     * @param tecnico the tecnico to set
+     */
+    public void setTecnico(Tecnico tecnico) {
+        this.tecnico = tecnico;
+    }
+
+    /**
+     * @return the valor
+     */
+    public double getValor() {
+        return valor;
+    }
+
+    /**
+     * @param valor the valor to set
+     */
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    /**
+     * @return the prioridade
+     */
+    public int getPrioridade() {
+        return prioridade;
+    }
+
+    /**
+     * @param prioridade the prioridade to set
+     */
+    public void setPrioridade(int prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    /**
+     * @return the tipo
+     */
+    public String getTipo() {
+        return tipo;
+    }
+
+    /**
+     * @param tipo the tipo to set
+     */
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the dataHora
+     */
+    public String getDataHora() {
+        return dataHora;
+    }
+
+    /**
+     * @param dataHora the dataHora to set
+     */
+    public void setDataHora(String dataHora) {
+        this.dataHora = dataHora;
+    }
+
+    /**
+     * @return the descricao
+     */
+    public String getDescricao() {
+        return descricao;
+    }
+
+    /**
+     * @param descricao the descricao to set
+     */
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    /**
+     * @return the pecas
+     */
+    public String getPecas() {
+        return pecas;
+    }
+
+    /**
+     * @param pecas the pecas to set
+     */
+    public void setPecas(String pecas) {
+        this.pecas = pecas;
+    }
     
 }
--- src/main/java/com/dougbaltazar/sgos/objs/Tecnico.java
+++ src/main/java/com/dougbaltazar/sgos/objs/Tecnico.java
@@ -14,5 +14,82 @@ public class Tecnico {
     private String nome;
     private int trabalhos;
     private String status;
+    private Contato contato;
+    public Tecnico(int i, String n, int t, String st, Contato c) {
+        this.id = i;
+        this.nome = n;
+        this.trabalhos = t;
+        this.status = st;
+        this.contato = c;
+    }
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the nome
+     */
+    public String getNome() {
+        return nome;
+    }
+
+    /**
+     * @param nome the nome to set
+     */
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    /**
+     * @return the trabalhos
+     */
+    public int getTrabalhos() {
+        return trabalhos;
+    }
+
+    /**
+     * @param trabalhos the trabalhos to set
+     */
+    public void setTrabalhos(int trabalhos) {
+        this.trabalhos = trabalhos;
+    }
+
+    /**
+     * @return the status
+     */
+    public String getStatus() {
+        return status;
+    }
+
+    /**
+     * @param status the status to set
+     */
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    /**
+     * @return the contato
+     */
+    public Contato getContato() {
+        return contato;
+    }
+
+    /**
+     * @param contato the contato to set
+     */
+    public void setContato(Contato contato) {
+        this.contato = contato;
+    }
     
 }
